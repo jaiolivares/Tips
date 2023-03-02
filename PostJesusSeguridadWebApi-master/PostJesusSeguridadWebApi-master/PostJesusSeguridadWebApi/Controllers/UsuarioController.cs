e32c6a934e7c1559b517c56d2afa72edc265278a
Javier Olivares Zavala <javier.olivares.zavala@gmail.com>
2023-03-01 21:18:16 -0300

    JOlivares
    Carga inicial

---
+++ PostJesusSeguridadWebApi-master/PostJesusSeguridadWebApi-master/PostJesusSeguridadWebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PostJesusSeguridadWebApi.Models;
+
+namespace PostJesusSeguridadWebApi.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        ModeloUsuario modelo;
+        public UsuarioController()
+        {
+            modelo = new ModeloUsuario();
+        }
+
+        [Authorize(Roles ="ADMINISTRADOR")]
+        public String Get()
+        {
+            return modelo.GetMensajeAdmin();
+        }
+
+       [HttpGet]
+        [Authorize(Roles = "USUARIO")]
+        [Route("api/currito")]
+        public String usuario()
+        {
+            return modelo.GetMensajeUsuario();
+        }
+    }
+}
+ 
