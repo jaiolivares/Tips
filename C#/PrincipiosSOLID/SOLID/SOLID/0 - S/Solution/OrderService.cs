ee1fa64397f29be00868bd61b6ceb82ea7b5f53a
Javier Olivares Zavala <javier.olivares.zavala@gmail.com>
2023-06-15 21:33:40 -0400

    PruebasLocales

--- C#/PrincipiosSOLID/SOLID/SOLID/0 - S/Solution/OrderService.cs
+++ C#/PrincipiosSOLID/SOLID/SOLID/0 - S/Solution/OrderService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SOLID._0___S.Solution
+﻿namespace SOLID._0___S.Solution
 {
     internal class OrderService
     {
--- C#/PrincipiosSOLID/SOLID/SOLID/Program.cs
+++ C#/PrincipiosSOLID/SOLID/SOLID/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SOLID._0___S.Solution;
+using SOLID._0___S.Model;
+
 Console.WriteLine("Hello, World!");
+
+// create dependencies
+var orderRepository = new OrderRepository();
+var notificationService = new NotificationService();
+var invoiceService = new InvoiceService();
+var loggerService = new LoggerService();
+
+// create OrderService instance
+var orderService = new OrderService(orderRepository, notificationService, invoiceService, loggerService);
+
+// create order
+var order = new Order();
+
+// call SaveOrder method
+orderService.SaveOrder(order);
