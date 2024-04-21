20178db370c2360b17904f773a1830bfafdbb7cf
Javier Olivares Zavala <javier.olivares.zavala@gmail.com>
2024-04-21 12:12:46 -0400

    Cargar resultado JSON desde query en SQL

---
+++ C#/08_Json_DesdeBD/JsonDesdeBD/JsonDesdeBD/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Windows.Services.Store;
+
+string connectionDb = "Data Source=JOLIVARESPC;Initial Catalog=StorageExample;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+var optionsBuilder = new DbContextOptionsBuilder<StoreContext>();
+optionsBuilder.UseSqlServer(connectionDb);
+
+using (var db = new StoreContext(optionsBuilder.Options))
+{
+    var result = db.Database.SqlQueryRaw<string>("" +
+        "SELECT Brands.Name as Name," +
+        "(SELECT Beers.Name" +
+        "FROM Beers" +
+        "WHERE Beers.BrandID = Brands.BrandID" +
+        "FOR JSON PATH)" +
+        "AS Beers" +
+        "FROM Brands" +
+        "FOR JSON PATH" +
+        "").ToList();
+
+    var brandBeers = JsonSerializer.Deserialize<List<Brand>>(result[0]);
+
+    foreach (var brand in brandBeers)
+    {
+        Console.WriteLine(brand.Name);
+
+        foreach (var beer in brand.Beers)
+        {
+            Console.WriteLine("--" + beer.Name);
+        }
+    }
+}
+
+internal class Brand
+{
+    public string Name { get; set; }
+    public List<Beer> Beers { get; set; }
+}
+
+internal class Beer
+{
+    public string Name { get; set; }
+}
---
+++ C#/08_Json_DesdeBD/README.md
@@ -0,0 +1,13 @@
+# Tips
+
+https://youtu.be/CiQFafuRAcE?t=728
+
+Canal: hdeleon.net
+
+¿Cómo RESOLVER el Problema del N+1? Tarde o temprano TE VA A TOCAR 💀
+
+El problema de N + 1 es un problema que tarde o temprano te vas a enfrentar como Programador, en este video te daré varias soluciones con sus ventajas y desventajas.
+
+---
+
+A partid del minuto 12:00
