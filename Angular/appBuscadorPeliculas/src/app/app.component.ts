25a9c20edb44c78a746fbde6d1b4ac0e765953fc
Javier Olivares Zavala <javier.olivares.zavala@gmail.com>
2024-05-22 20:18:21 -0400

    App buscador de peliculas (básico) y ejercicios básicos

--- Angular/appBuscadorPeliculas/src/app/app.component.ts
+++ Angular/appBuscadorPeliculas/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appBuscadorPeliculas';
+  variable_interpolada:string = "App de Peliculas - Interpolada"
+  objetoUnico = {
+    id: 1,
+    nombre: "Mi Nombre"
+  }
+
+  array = [
+    {
+      id: 1, Nombre: "Nombre uno"
+    },
+    {
+      id:2, Nombre: "Nombre dos"
+    },
+    {
+      id:3, Nombre: "Nombre tre"
+    }
+  ]
 }
--- Angular/appBuscadorPeliculas/src/app/peliculas/peliculas.component.ts
+++ Angular/appBuscadorPeliculas/src/app/peliculas/peliculas.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MovieAppService } from '../services/movie-app.service';
 
+import { Persona } from '../interfaces/persona';
+
 @Component({
   selector: 'app-peliculas',
   templateUrl: './peliculas.component.html',
@@ -10,11 +12,34 @@ export class PeliculasComponent {
   movies: any[] = [];
   searchTerm: string = '';
 
-  constructor(private movieService: MovieAppService) { }
+  persona:Persona={
+    nombre: "Javier Olivares",
+    id: 99,
+    estado: false
+ }
 
-  searchMovies() {
-    this.movieService.buscarPelicula(this.searchTerm).subscribe((data: any) => {
-      this.movies = data.results;
-    });
+ objPersonas: Persona[] = [{nombre: "Alan Brito", id: 11, estado: true}];
+
+ constructor(private movieService: MovieAppService) { 
+  this.objPersonas.push({nombre: "Desde constructor", id: 13, estado: true});
+ }
+
+ ngOnInit(){
+  this.objPersonas.push({nombre: "Desde init", id: 13, estado: true});
+  this.loadObjPersonas();
+ }
+ 
+ searchMovies() {
+   this.movieService.buscarPelicula(this.searchTerm).subscribe((data: any) => {
+     this.movies = data.results;
+   });
+ }
+  
+ loadObjPersonas() {
+   this.objPersonas.push({nombre: "Alan", id: 13, estado: true})
+   this.objPersonas.push({nombre: "Brito", id: 14, estado: true})
   }
+
+
+
 }
